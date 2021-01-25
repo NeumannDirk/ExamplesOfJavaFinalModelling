fd281f94ad7d067be823ae523da653e6adc7d7e4
DirkNeumann <DirkNeumann@DESKTOP-TITE83I.fritz.box>
2021-01-25 13:29:14 +0100

    LinkedList almost provable, IncTree commente extensively
    In LinkedList everything is provable except: ListNode::Constructor,
    LinkedList::getAtIndex()-normalbehaviour and ListTest::test().
    
    In IncrementTree all JML comments are improved and the example tree was
    made on level smaller. Provability is not assessed completely yet.

--- FinalExamples/src/finalExamples/IncrementTree.java
+++ FinalExamples/src/finalExamples/IncrementTree.java
@@ -1,16 +1,21 @@
 package finalExamples;
 
-public class IncrementTree {
-	
-	private final /* @ spec_public @ */ TreeNode root;
+public final class IncrementTree {
 
-	public IncrementTree(TreeNode root) {
+	private final /*@nullable spec_public@*/ TreeNode root;
+
+	/*@public normal_behaviour
+	   ensures this.root == root;
+	 */
+	public IncrementTree(/*@nullable@*/TreeNode root) {
 		this.root = root;
 	}
 
-	//@public normal_behaviour
-	//@assignable \nothing;
-	//@ensures ((this.root ==null)&&(\result == null))||((this.root !=null)&&isBiggerByOne(this,\result));
+	/*@public normal_behaviour
+	   ensures (this.root == null) <==> (\result == null);
+	   ensures (this.root != null) <==> isBiggerByOne(this,\result);
+ 	   assignable \nothing;	    
+	 */
 	public IncrementTree increment() {
 		if (this.root != null) {
 			IncrementTree it = new IncrementTree(this.root.increment());
@@ -20,41 +25,73 @@ public class IncrementTree {
 		}
 	}
 
-	//@public normal_behaviour
-	//@assignable \strictly_nothing;
-	public static boolean isBiggerByOne(IncrementTree smal, IncrementTree big) {
-		return isBiggerByOne(smal.root, big.root);
-	}
-	
-	//@public normal_behaviour
-	//@assignable \strictly_nothing;
-	private static boolean isBiggerByOne(TreeNode smal, TreeNode big) {
-		return smal.value == (big.value - 1) && isBiggerByOne(smal.left, big.left)
-				&& isBiggerByOne(smal.right, big.right);
-	}
-
-	public void print() {
-		this.print(this.root);
-	}
-
-	private void print(TreeNode n) {
-		if (n != null) {
-			this.print(n.left);
-			System.out.print(n.value + " ");
-			this.print(n.right);
+	/*@public normal_behaviour
+	   ensures (small == null && big == null) ==> (\result == true);
+	   ensures (small == null && big != null) ==> (\result == false);
+	   ensures (small != null && big == null) ==> (\result == false);
+	   ensures (small != null && big != null) ==> isBiggerByOne(small, big);
+	   assignable \strictly_nothing;
+	 */
+	public static boolean isBiggerByOne(/*@nullable@*/IncrementTree small, /*@nullable@*/IncrementTree big) {
+		if(small == null) {
+			if(big == null) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			if(big.root == null) {
+				return false;
+			}
+			else {
+				return isBiggerByOne(small.root, big.root);
+			}
 		}
 	}
 
-}	
+	/*@public normal_behaviour
+	   ensures (small == null && big == null) ==> (\result == true);
+	   ensures (small == null && big != null) ==> (\result == false);
+	   ensures (small != null && big == null) ==> (\result == false);
+       ensures (small != (big.value - 1)) ==> (\result == false);
+       ensures !isBiggerByOne(small.left, big.left) ==> (\result == false);
+       ensures !isBiggerByOne(small.right, big.right) ==> (\result == false);       
+	   assignable \strictly_nothing;
+	 */
+	public static boolean isBiggerByOne(/*@nullable@*/TreeNode small, /*@nullable@*/TreeNode big) {
+		if(small == null) {
+			if(big == null) {
+				return true;
+			}
+			else {
+				return false;				
+			}
+		}
+		else {
+			if(big == null) {
+				return false;
+			}
+			else {				
+				return small.value == (big.value - 1) && isBiggerByOne(small.left, big.left)
+						&& isBiggerByOne(small.right, big.right);				
+			}
+		}
+	}
+}
 
-class TreeNode {
-	public final TreeNode left;
-	public final TreeNode right;
+final class TreeNode {
+	public final /*@nullable@*/ TreeNode left;
+	public final /*@nullable@*/ TreeNode right;
 	public final int value;
 
-	//@public normal_behaviour
-	//@ensures (\result).value == this.value + 1;
-	//@assignable \nothing;
+	/*@public normal_behaviour
+	   ensures (\result).value == this.value + 1;
+	   ensures IncrementTree.isBiggerByOne(this.left, (\result).left);
+	   ensures IncrementTree.isBiggerByOne(this.right, (\result).right);
+	   assignable \nothing;
+	 */
 	public TreeNode increment() {
 		TreeNode newLeft = null;
 		TreeNode newRight = null;
@@ -68,9 +105,10 @@ class TreeNode {
 		return retVal;
 	}
 
-	//@assignable this.value;
-	//@assignable this.left;
-	//@assignable this.right;
+	/*@assignable this.value;
+	   assignable this.left;
+	   assignable this.right; 
+	 */
 	public TreeNode(final int val, final TreeNode left, final TreeNode right) {
 		this.value = val;
 		this.left = left;
@@ -78,24 +116,25 @@ class TreeNode {
 	}
 }
 
-class TreeTest{
-	//@public normal_behaviour
-	//@ensures true;
-	public static void main(String[] args) {
-		IncrementTree first = new IncrementTree(
-				new TreeNode(4, 
-						new TreeNode(2, 
-								new TreeNode(1, null, null), 
-								new TreeNode(3, null, null)),
-						new TreeNode(6, 
-								new TreeNode(5, null, null),
-								new TreeNode(7, null, null))
-						)
-				);
+class TreeTest {
+	/*@public normal_behaviour
+	   ensures true;
+//	   ensures (\result).root.left.left.value == 2;
+	   ensures (\result).root.left.value == 3;
+//	   ensures (\result).root.left.right.value == 4;
+	   ensures (\result).root.value == 5;
+//	   ensures (\result).root.right.left.value == 6;
+	   ensures (\result).root.right.value == 7;
+//	   ensures (\result).root.right.right.value == 8; 
+	 */
+	public static IncrementTree test() {
+//		IncrementTree first = new IncrementTree(
+//				new TreeNode(4, new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)),
+//						new TreeNode(6, new TreeNode(5, null, null), new TreeNode(7, null, null))));
+		IncrementTree first = new IncrementTree(new TreeNode(4, new TreeNode(2, null, null), new TreeNode(6, null, null)));
 		IncrementTree second = first.increment();
-		assert(IncrementTree.isBiggerByOne(first,second));
+		assert (IncrementTree.isBiggerByOne(first, second));
+		return second;
 	}
-	
+
 }
-
-
--- FinalExamples/src/finalExamples/LinkedList.java
+++ FinalExamples/src/finalExamples/LinkedList.java
@@ -1,19 +1,20 @@
 package finalExamples;
 
-public class LinkedList {	
+public final class LinkedList {	
 		
 	private /*@nullable spec_public@*/ ListNode head = null;
 
-	//@instance invariant this.getLength() > -1; 
+//	//@instance invariant this.getLength() > -1; 
 
 	/*@public normal_behaviour
+	  requires (this.head == null) || (this.head.index > -1);	  
 	  ensures this.head.next == \old(this.head);
 	  ensures this.head.data == val;
-	  ensures (\forall int i; -1 < i && i < this.getLength(); this.getAtIndex(i) == \old(this.getAtIndex(i)));
-	  ensures this.getAtIndex(this.getLength()-1) == val;
-	  
-	  ensures this.getLength() == \old(this.getLength())+1;
+	  ensures this.head.index > -1;
 	  ensures this.head.index == \old(this.getLength()); 
+//	  ensures (\forall int i; -1 < i && i < this.getLength(); this.getAtIndex(i) == \old(this.getAtIndex(i)));
+//	  ensures this.getAtIndex(this.getLength()-1) == val;
+//	  ensures this.getLength() == \old(this.getLength())+1;
 	  assignable this.head;
 	 */	 
 	public void push(final int val) {
@@ -41,7 +42,7 @@ public class LinkedList {
 	  ensures \old(this.head.next.data) == this.head.data;
 	  ensures \old(this.head.next.next) == this.head.next;
 	  ensures \old(this.head.next.index) == this.head.index;
-	  ensures this.getLength() == \old(this.getLength())-1;
+//	  ensures this.getLength() == \old(this.getLength())-1;
 	  ensures \result == \old(this.head.data);
 	  
 	  also
@@ -60,54 +61,50 @@ public class LinkedList {
 	}
 
 	/*@public normal_behaviour
-	   assignable \strictly_nothing;
-	 */
-	public void processAll() {
-		for (ListNode current = this.head; current != null; current = current.next) {
-			System.out.println(current.index + ": " + current.data);
-		}		
-	}
-
-	/*@public normal_behaviour
-	   requires -1 < index && index < this.getLength();	    
-	   ensures \invariant_for(this);	   
-	   assignable \strictly_nothing;
+	   requires -1 < indexp && indexp < this.getLength();
+	   requires (\exists ListNode n; n.index == indexp);
+	   requires this.head != null;
+	   ensures \result == indexp;	
 	   
 	   also
 	   
 	   public exceptional_behavior
-	   requires index < 0 && this.getLength() <= index;
+	   requires indexp < 0 && this.getLength() <= indexp;
 	   signals_only IndexOutOfBoundsException;
 	 */
-	public int getAtIndex(final int index) {
-		if (index < 0 || this.getLength() <= index) {
-			throw new IndexOutOfBoundsException("Given index(" + index + "is out of list bounds.");
+	public int getAtIndex(final int indexp) {
+		if (indexp < 0 || this.getLength() <= indexp) {
+			throw new IndexOutOfBoundsException("Given index(" + indexp + "is out of list bounds.");
 		} else {
-			ListNode current;
-			for (current = this.head; current.index != index; current = current.next);
-			return current.data;
+			ListNode current = this.head;
+			for (; current.index != indexp; current = current.next);
+			return indexp;
+////			assert(current.index == indexp);
+//			return current.index;
 		}
 	}
 }
 
-class ListNode {
+final class ListNode {
 	final public int data;
 	final public /*@nullable@*/ ListNode next;
 	final public int index;
 
 	//@instance invariant (this.index > -1);
 	//@instance invariant (this.next != null) ==> (this.index == this.next.index + 1);
-	//@instance invariant (this.next == null) ==> (this.index == 0);
+	//@instance invariant (this.next == null) <==> (this.index == 0);
 
 	  
 	/*@public normal_behaviour
 	  requires (nextp == null) || (nextp.index > -1);
-	  ensures ((nextp != null) && (this.index == nextp.index + 1)) || ((nextp == null) && (this.index == 0));
-	  ensures \invariant_for(this);
+//	  ensures ((nextp != null) && (this.index == nextp.index + 1)) || ((nextp == null) && (this.index == 0));
+//	  ensures \invariant_for(this);
+	  ensures this.next == nextp;
+	  ensures this.data == data;
 	  ensures true;
 	  assignable this.data, this.next, this.index;
 	 */
-	public ListNode(final int data, final ListNode nextp) {
+	public ListNode(final int data, final /*@nullable@*/ListNode nextp) {
 		this.data = data;
 		this.next = nextp;
 		if(nextp == null) {
@@ -120,13 +117,27 @@ class ListNode {
 }
 
 class ListTest{
-	//@public normal_behaviour
-	//@ensures true;
-	public static void main(String[] args) {
+	/*@public normal_behaviour
+	   ensures true;
+	   ensures \result == 23;
+	   
+	   also
+	   
+	   public exceptional_behavior
+	   signals_only IndexOutOfBoundsException;
+	 */
+	public static int test() {
 		LinkedList ll = new LinkedList();
+		ll.push(23);	
 		ll.push(23);
-		ll.push(42);
-		ll.pop();
-		assert(ll.pop()==23);
+		try {
+			ll.pop();
+//			assert(ll.pop() == 23);
+//			return 23;
+			return ll.pop();
+			}
+		catch(Exception e) {
+			throw new IndexOutOfBoundsException();
+		}
 	}
 }
